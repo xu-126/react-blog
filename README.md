939937c8e407771ee0747cab2bb2922f18d64db6
xujiaying <1264930795@qq.com>
2021-02-07 15:05:03 +0800

    feat: 接口中台

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # react-blog
+
 基于react-hooks egg.js 开发的个人博客系统
+
+## blog 前台
+
+用户使用，博客展现
+
+## service 使用egg.js搭建接口中台
+
+数据接口，业务逻辑
+egg.js 底层即为 koa
+
+## 后台管理
+
+文章类别管理，系统设置
--- blog/pages/detailed.js
+++ blog/pages/detailed.js
@@ -1,6 +1,6 @@
 import React,{useState} from 'react'
 import Head from 'next/head'
-import {Row, Col , Icon ,Breadcrumb  } from 'antd'
+import {Row, Col , Icon , Affix, Breadcrumb  } from 'antd'
 
 import Header from '../components/Header'
 import Author from '../components/Author'
@@ -8,7 +8,50 @@ import Advert from '../components/Advert'
 import Footer from '../components/Footer'
 import '../styles/pages/detailed.css'
 
+import ReactMarkdown from 'react-markdown'
+import MarkNav from 'markdown-navbar';
+import 'markdown-navbar/dist/navbar.css';
+
+
+
 const Detailed = () => {
+  let markdown=
+  '# p01:来个Hello World 初始Vue3.0\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n'+
+  '***\n\n\n' +
+  '# p001:来个Hello World 初始Vue3.0\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n'+
+  '***\n\n\n' +
+  '# p02:来个Hello World 初始Vue3.0\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n'+
+  '***\n\n\n' +
+  '# p03:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '# p04:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '# p05:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '# p06:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '# p07:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '``` var a=11; ```'
   return (
     <>
       <Head>
@@ -38,7 +81,10 @@ const Detailed = () => {
                 </div>
 
                 <div className="detailed-content" >
-                  详细内容，下节课编写
+                  <ReactMarkdown 
+                    source={markdown} 
+                    escapeHtml={false}  
+                  />
                 </div>
 
              </div>
@@ -49,7 +95,16 @@ const Detailed = () => {
         <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
           <Author />
           <Advert />
-
+          <Affix offsetTop={5}>
+            <div className="detailed-nav comm-box">
+              <div className="nav-title">文章目录</div>
+              <MarkNav
+                className="article-menu"
+                source={markdown}
+                ordered={false}
+              />
+            </div>
+          </Affix>
         </Col>
       </Row>
       <Footer/>
